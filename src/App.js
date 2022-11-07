58ce499cc3e8f869af8ba7d3283112417e24d035
Gabriel Ramos <gabrielramos1764@gmail.com>
2022-11-07 17:44:40 -0300

    Portada portafolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import {React, useState, useEffect } from 'react';
+import Cover from './component/Cover/Cover';
 import './App.css';
+import Barra from './component/Barra/barra';
 
 function App() {
+  const [scroll,setScroll]= useState(0);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScroll(position);
+  }
+
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);    
+  }, [scroll]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Barra isScrolling={scroll}></Barra>
+      <Cover></Cover>
+      
     </div>
   );
 }
